b79c9c2f03da241830553b8eb7bedf75e1ca68e2
marchesed <marchesedb@gmail.com>
2022-11-12 16:04:40 -0500

    added horizontal pan gestures

--- screens/GraphViewer.js
+++ screens/GraphViewer.js
@@ -2,7 +2,7 @@ import React from "react";
 import { View, Text, Dimensions, StyleSheet, Alert} from "react-native";
 import { StatusBar } from 'expo-status-bar';
 import { Gesture, GestureDetector } from 'react-native-gesture-handler';
-import Animated, { useSharedValue, useAnimatedStyle, withDelay } from 'react-native-reanimated';
+import Animated, { useSharedValue, useAnimatedStyle, withTiming } from 'react-native-reanimated';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
@@ -13,6 +13,10 @@ export default function GraphViewer({ route, navigation }) {
 
     const scale = useSharedValue(1);
     const savedScale = useSharedValue(1);
+    const END_POSITION = 200;
+    const onLeft = useSharedValue(true);
+    const onRight = useSharedValue(true);
+    const leftPosition = useSharedValue(0);
 
     const pinchGesture = Gesture.Pinch()
     .onUpdate((e) => {
@@ -21,6 +25,7 @@ export default function GraphViewer({ route, navigation }) {
     .onEnd(() => {
         if (scale.value < 1) {
             scale.value = 1;
+            leftPosition.value = 0;
         }
         savedScale.value = scale.value;
     });
@@ -32,15 +37,33 @@ export default function GraphViewer({ route, navigation }) {
         if (scale.value <= 1) {
             scale.value = savedScale.value * 2;
         } else {
+            leftPosition.value = 0;
             scale.value = 1;
         }
         savedScale.value = scale.value;
     });
 
-    const composed = Gesture.Simultaneous(pinchGesture, doubleTap);
+    const panGesture = Gesture.Pan()
+    .onUpdate((e) => {
+        if (scale.value !== 1) {
+            if (onLeft.value) {
+                leftPosition.value = e.translationX;
+            } else {
+                leftPosition.value = END_POSITION + e.translationX;
+            }
+        }
+    })
+    .onEnd((e) => {
+        if (leftPosition.value < -200 || leftPosition.value > 200) {
+            leftPosition.value = withTiming(0, { duration: 100 });
+            onLeft.value = true;
+        }
+    });
+
+    const composed = Gesture.Simultaneous(pinchGesture, panGesture, doubleTap);
 
     const animatedStyle = useAnimatedStyle(() => ({
-        transform: [{ scale: scale.value }],
+        transform: [{ scale: scale.value }, { translateX: leftPosition.value }],
       }));
 
     return(
