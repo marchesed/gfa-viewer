d1bb85030fdd80c2c0abd8cc3763c18347415d0b
marchesed <marchesedb@gmail.com>
2022-11-12 16:29:36 -0500

    make clock live

--- components/Clock.js
+++ components/Clock.js
@@ -1,12 +1,19 @@
+import { useEffect, useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 
 export default function Clock () {
-    let now = new Date();
+
+    const [date, setDate] = useState(new Date())
+
+    useEffect(() => {
+        setInterval(() => setDate(new Date()), 30000);
+    }, []);
+
     return (
         <View style={styles.timeContainer}>
-            <Text>Current Zulu time: {now.getUTCHours()}:{now.getUTCMinutes()}</Text> 
-            <Text>Current Local time: {now.getHours()}:{now.getMinutes()}</Text>
+            <Text>Current Zulu time: {date.getUTCHours()}:{date.getUTCMinutes()}</Text> 
+            <Text>Current Local time: {date.getHours()}:{date.getMinutes()}</Text>
         </View>
     )
 }
