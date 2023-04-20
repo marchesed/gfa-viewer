c6d0f57ae3dbdaebd67973bf74f4fdf4cc70fa39
marchesed <marchesedb@gmail.com>
2023-04-20 13:25:53 -0400

    fix expo v48 upgrade issues with random values

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './screens/Home';
@@ -10,6 +8,7 @@ import {
 } from '@segment/analytics-react-native';
 import { LogBox } from 'react-native';
 import * as Sentry from '@sentry/react-native';
+import 'react-native-get-random-values';
 
 Sentry.init({ 
   dsn: 'https://13c39a33c6f1459c91742be51513b473@o4504941124255744.ingest.sentry.io/4504941125500928',
